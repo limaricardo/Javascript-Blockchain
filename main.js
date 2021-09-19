b0bb18dc089e7df09ea097f2c5e4ce51549f6332
Ricardo <limaricardop@gmail.com>
2021-09-18 23:18:14 -0300

    Proof-of-work

--- main.js
+++ main.js
@@ -1,66 +1,73 @@
-const SHA256 = require('crypto-js/sha256')
+const SHA256 = require("crypto-js/sha256");
 
-class Block{
-    constructor(index, timestamp, data, previousHash = ''){
-        this.index = index;
-        this.timestamp = timestamp;
-        this.data = data;
-        this.previousHash = previousHash;
+class Block {
+  constructor(index, timestamp, data, previousHash = "") {
+    this.index = index;
+    this.timestamp = timestamp;
+    this.data = data;
+    this.previousHash = previousHash;
+    this.hash = this.calculateHash();
+    this.nonce = 0;
+  }
+
+  calculateHash() {
+    return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data) + this.nonce).toString();
+  }
+
+  mineBlock(difficulty) {
+    while (this.hash.substring(0, difficulty) !== Array(difficulty + 1).join("0")) {
+        this.nonce++;
         this.hash = this.calculateHash();
     }
 
-    calculateHash(){
-        return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data)).toString();
-    }
+    console.log("Block mined: " + this.hash);
+  }
 }
 
-class Blockchain{
-    constructor(){
-        this.chain = [this.createGenesisBlock()];
+class Blockchain {
+  constructor() {
+    this.chain = [this.createGenesisBlock()];
+    this.difficulty = 4;
+  }
+
+  createGenesisBlock() {
+    return new Block(0, "13/09/2021", "Genesis block", "0");
+  }
+
+  getLatestBlock() {
+    return this.chain[this.chain.length - 1];
+  }
+
+  addBlock(newBlock) {
+    newBlock.previousHash = this.getLatestBlock().hash;
+    newBlock.mineBlock(this.difficulty);
+    this.chain.push(newBlock);
+  }
+
+  isChainValid() {
+    for (let i = 1; i < this.chain.length; i++) {
+      const currentBlock = this.chain[i];
+      const previousBlock = this.chain[i - 1];
+
+      if (currentBlock.hash !== currentBlock.calculateHash()) {
+        return false;
+      }
+
+      if (currentBlock.previousHash !== previousBlock.hash) {
+        return false;
+      }
     }
 
-    createGenesisBlock(){
-        return new Block(0, "13/09/2021", "Genesis block", "0");
-    }
-
-    getLatestBlock(){
-        return this.chain[this.chain.length - 1];
-    }
-
-    addBlock(newBlock){
-        newBlock.previousHash = this.getLatestBlock().hash;
-        newBlock.hash = newBlock.calculateHash();
-        this.chain.push(newBlock);
-    }
-
-    isChainValid(){
-        for(let i = 1; i < this.chain.length; i++){
-            const currentBlock = this.chain[i];
-            const previousBlock = this.chain[i - 1];
-
-            if(currentBlock.hash !== currentBlock.calculateHash()){
-                return false;
-            }
-
-            if(currentBlock.previousHash !== previousBlock.hash){
-                return false;
-            }
-        }
-
-        return true;
-    }
+    return true;
+  }
 }
 
 let limaCoin = new Blockchain();
-limaCoin.addBlock(new Block(1, "14/09/2021", { amount: 4}));
-limaCoin.addBlock(new Block(2, "16/09/2021", { amount: 10}));
 
-console.log("Is blockchain valid? " + limaCoin.isChainValid());
+console.log("Mining block 1...");
+limaCoin.addBlock(new Block(1, "14/09/2021", { amount: 4 }));
 
-limaCoin.chain[1].data = { amount: 100 };
-limaCoin.chain[1].hash = limaCoin.chain[1].calculateHash();
+console.log("Mining block 2...");
+limaCoin.addBlock(new Block(2, "16/09/2021", { amount: 10 }));
 
 
-console.log("Is blockchain valid? " + limaCoin.isChainValid());
-
-// console.log(JSON.stringify(limaCoin, null, 4));
